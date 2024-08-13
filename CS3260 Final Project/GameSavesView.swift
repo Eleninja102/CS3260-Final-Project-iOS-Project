de88cfb73f2211585538f570f90dbe38567ffffe
Coleton Watt <electricninja102@gmail.com>
2024-08-12 23:06:14 -0600

    Bug fixes

--- CS3260 Final Project/GameSavesView.swift
+++ CS3260 Final Project/GameSavesView.swift
@@ -15,7 +15,7 @@ struct GameSavesView: View {
 	@Environment(\.modelContext) var modelContext
 	@EnvironmentObject var gameKitTool: GameKitTool
 	@State private var showingSheet = false
-	@Query(sort: \GameData.creationDate) var gameData: [GameData]
+	@Query(sort: \GameData.creationDate, order: .reverse) var gameData: [GameData]
 	@State var gameStated = true
 	
 	
@@ -23,7 +23,6 @@ struct GameSavesView: View {
 	/// - Parameter indexSet: the index set of the delete
 	func deleteGameData(_ indexSet: IndexSet){
 		for index in indexSet{
-			let gameToDelete = gameData[index]
 			modelContext.delete(gameData[index])
 			do {
 			   try modelContext.save()
@@ -80,6 +79,7 @@ struct GameSavesView: View {
 		let config = ModelConfiguration(isStoredInMemoryOnly: true)
 		let container = try ModelContainer(for: GameData.self, configurations: config)
   		let testGame = GameData(matchName: "Nothing", userPlayer: PlayerDetails(playerName: "Coleton", gamePlayerID: "Coleton2", teamPlayerID: "Coleton"), oppenents: [PlayerDetails(playerName: "Bob", gamePlayerID: "Bob", teamPlayerID: "Bo2b")])
+		
 		container.mainContext.insert(testGame)
 		
 		return GameSavesView()
@@ -102,29 +102,61 @@ struct saveListItem: View{
 			VStack(alignment: .leading){
 				Text("\(save.matchName)")
 				Text(save.creationDate.formatted(date: .long, time: .omitted))
-				Text("Player: \(save.userPlayer.playerName) VP: \(save.userPlayer.vp)")
+				Text("Player: \(save.userPlayer.playerName) VP: \(save.userPlayer.realVp)")
 				HStack{
 					Text("Other Players:")
 					ForEach(save.opponents){ player in
 						HStack{
-							Text("\(player.playerName) - \(player.vp)")
+							Text("\(player.playerName) - \(player.realVp)")
 						}
 						
 					}
 				}
 			}
 			Spacer()
-			Button{
-				gameKitTool.startMatchmaking(gameData: save)
+			NavigationLink{
+				EditSaveView(newSaveName: save.matchName, gameData: save)
 
 			}label: {
-				Image(systemName: "arrowtriangle.forward")
-					.bold()
+				
+			}
+			.frame(width: 1.0, height: 1.0)
+		}
+	}
+}
+struct EditSaveView: View {
+	@Environment(\.modelContext) var modelContext
+	@EnvironmentObject var gameKitTool: GameKitTool
+	@State var newSaveName: String
+	@Bindable var gameData: GameData
+	var body: some View {
+		Form{
+			Section{
+				TextField(text: $newSaveName){
+					Text("Save Name")
+				}
+			}header: {
+				Text("Save Name")
+			}
+			
+			Section{
+				Text("\(gameKitTool.user?.displayName ?? "")")
+			}header: {
+				Text("Player Details")
+			}
+		}
+		.toolbar{
+			ToolbarItem{
+				Button{
+					gameKitTool.startMatchmaking(gameData: gameData)
+				}label: {
+					Text("Start Game")
+				}
+				.disabled(newSaveName == "")
 			}
 		}
 	}
 }
-
 struct NewSaveView: View{
 	@Environment(\.modelContext) var modelContext
 	@EnvironmentObject var gameKitTool: GameKitTool
